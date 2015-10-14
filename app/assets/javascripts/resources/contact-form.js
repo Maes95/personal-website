49d97f1366554b6e891c93bc2cc37e896441a6a0
Javier <javier@rmrz.me>
2015-10-14 23:20:39 +0200

    Added contact form front-end functionality

--- app/assets/javascripts/resources/contact-form.js
+++ app/assets/javascripts/resources/contact-form.js
@@ -1,6 +1,6 @@
 angular.module('app')
   .factory('ContactForm', function ContactFormFactory($resource) {
-    return $resource('/api/contacts/:id', {}, {
+    return $resource('/api/contact_form/:id', {}, {
       update: {
         method: "PUT"
       }
